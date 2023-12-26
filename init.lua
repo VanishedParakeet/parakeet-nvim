c17bdfcadde7b6f0ffbf27ac5e70c6c96eca06a8
VanishedParakeet <99857464+VanishedParakeet@users.noreply.github.com>
2023-12-26 13:56:56 -0700

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,30 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set number")
+vim.cmd("set autoindent")
+vim.cmd("imap jj <Esc>")
+vim.cmd("set colorcolumn=80")
+vim.cmd("set wildmenu")
+vim.cmd("set wildchar=<Tab>")
+vim.cmd("set wildmode=list:longest,full")
+
+vim.g.mapleader = " "
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local opts = {}
+
+require("lazy").setup("plugins")
